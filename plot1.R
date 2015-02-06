2378267b1b795548c25c2f6bba96147d989b4921
Nazaret <nazasan@hotmail.com>
2015-02-06 20:19:28 +0100

    commit

--- plot1.R
+++ plot1.R
@@ -1,13 +1,4 @@
-plot1 <- function() {
-   ## if(!file.exists("data")) {
-    ##    dir.create("data")
-      ##  temp <- tempfile()
-        ##fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-        ##download.file(fileUrl,temp, method ="curl")
-        ##data <- read.table(unz(temp, "./data/household.txt"))
-        ##unlink(temp)
-    ##}
-    
+plot1 <- function() {    
    data <- read.table("./data/household_power_consumption.txt",header=TRUE,sep=";", colClasses = 
                                c("Date","character","numeric","numeric","numeric","numeric",
                                  "numeric","numeric","numeric"), na.strings="?")
